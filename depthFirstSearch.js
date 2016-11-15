1f41ad003a2c0878186ed744cda8de5f9c9152dc
Casey Oeda <coeda@live.com>
2016-11-15 13:33:49 -1000

    completed first 8 tests

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -1,9 +1,23 @@
 'use strict';
 
 const Node = require('./graphGenerator');
-
+var queue = [];
 const DFS = (start, searchFor) => {
+  queue.push(start);
+  if (start.value === searchFor){
+    return start.name;
+  } else if (start.neighbors !== undefined){
+    if(start.neighbors.length > 1){
 
-}
+    } else {
+      start = start.neighbors[0];
+      DFS(start, searchFor);
+    }
+  } else if (start.neightbors === undefined){
+    queue.pop();
+    start = queue[queue.length -1];
+    DFS(start, searchFor);
+  }
+};
 
 module.exports = DFS;
--- graphGenerator.js
+++ graphGenerator.js
@@ -1,8 +1,14 @@
 // Implement a graph generator that represents a set of objects where some pairs of objects are connected by links.
 
+
 const Node = function(name, value) {
+  this.name=name;
+  this.value=value;
+  this.neighbors=[];
+};
 
-}
-
+Node.prototype.addNeighbors = function (array) {
+    return this.neighbors = this.neighbors.concat(array);
+  };
 
 module.exports = Node;
