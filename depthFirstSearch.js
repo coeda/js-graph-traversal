371159e31efb3cf125605a6b6e64ca7b459e5a2e
Casey Oeda <coeda@live.com>
2016-11-15 15:42:52 -1000

    got DFS working

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -1,23 +1,42 @@
 'use strict';
 
 const Node = require('./graphGenerator');
-var queue = [];
+var stack = [];
+var completed = [];
+var found = false;
+var foundValue;
 const DFS = (start, searchFor) => {
-  queue.push(start);
+  stack.push(start);
   if (start.value === searchFor){
-    return start.name;
-  } else if (start.neighbors !== undefined){
-    if(start.neighbors.length > 1){
-
-    } else {
-      start = start.neighbors[0];
-      DFS(start, searchFor);
+    found = true;
+    foundValue = start;
+    return foundValue;
+  } else {
+    if (start.neighbors.length > 0){
+      if(start.neighbors.length > 1){
+        start.neighbors.forEach((value)=>{
+          if(completed.indexOf(value) === -1){
+            start = value;
+            DFS(start, searchFor);
+          }
+        });
+      } else {
+        start = start.neighbors[0];
+        DFS(start, searchFor);
+      }
+    } else if(start.neighbors.length === 0) {
+      var lastValue = stack.pop();
+      completed.push(lastValue);
+      if (stack.length === 0 && found === false){
+        foundValue = false;
+        return foundValue;
+      }
     }
-  } else if (start.neightbors === undefined){
-    queue.pop();
-    start = queue[queue.length -1];
-    DFS(start, searchFor);
   }
+  stack = [];
+  completed = [];
+  found = false;
+  return foundValue;
 };
 
 module.exports = DFS;
